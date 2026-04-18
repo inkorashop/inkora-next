bb7097b39524044efd0c45f045a4950339d79747
INKORA <inkorashop@gmail.com>
2026-04-17 23:52:34 -0300

    deploy

--- app/page.js
+++ app/page.js
@@ -462,8 +462,8 @@ export default function Home() {
                     onMouseLeave={() => setCardHover(prev => ({ ...prev, [d.id]: false }))}
                   >
                     <div style={{...s.cardImg, aspectRatio: cardAspectRatio}}>
-                      {d.model_url && activeProduct?.allow_3d
-                        ? <ModelViewer url={d.model_url} />
+                      {d.model_url
+                        ? <ModelViewer url={d.model_url} autoRotate={activeProduct?.allow_3d === true} />
                         : d.image_url
                         ? <img src={d.image_url} alt={d.name} style={{...s.img, objectFit: 'contain'}} />
                         : <span style={{fontSize:36}}>🎨</span>}
