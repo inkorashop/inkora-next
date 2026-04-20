6ae422ff53fadcf392a21e43515f93b77c9e6aec
INKORA <inkorashop@gmail.com>
2026-04-20 09:12:39 -0300

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  api: {
+    bodyParser: {
+      sizeLimit: '4mb',
+    },
+  },};
 
 export default nextConfig;
