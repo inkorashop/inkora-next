ac401541c2deb79b36665e8718b227f1be94ab73
INKORA <inkorashop@gmail.com>
2026-04-27 16:21:23 -0300

    deploy

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,7 @@ export const metadata = {
   },
 };
 
-export default function RootLayout({ children }) {
+export default functin RootLayout({ children }) {
   return (
     <html lang="es" className={barlow.className}>
       <body><CartProvider>{children}</CartProvider></body>
